761389b174d7f02b61a6bc45579e49bf6daa1cdf
Andrei Nemeti <andrein.webdesign@gmail.com>
2025-11-11 18:04:41 +0200

    refactored nav links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { NAV_LINKS } from './navLinks';
+import { NAV_LINKS } from '../utils/constants';
 
 export default function Nav() {
   const pathname = usePathname();
