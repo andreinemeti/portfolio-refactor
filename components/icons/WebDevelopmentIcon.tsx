dd451a64239e7080cf4c9298e1836feb7ff77b71
Andrei Nemeti <andrein.webdesign@gmail.com>
2025-11-14 12:16:55 +0200

    icons fix - bad markup

--- components/icons/WebDevelopmentIcon.tsx
+++ components/icons/WebDevelopmentIcon.tsx
@@ -5,7 +5,7 @@ export default function UiIcon(
   { size = 26, ...props }: React.SVGProps<SVGSVGElement> & { size?: number }
 ) {
   return (
-   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-code-xml w-12 h-12 text-orange-400 mb-4"><path d="m18 16 4-4-4-4"></path><path d="m6 8-4 4 4 4"></path><path d="m14.5 4-5 16"></path></svg>
+   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffb546" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-code-xml w-12 h-12 text-orange-400 mb-4"><path d="m18 16 4-4-4-4"></path><path d="m6 8-4 4 4 4"></path><path d="m14.5 4-5 16"></path></svg>
   );
 }
 
